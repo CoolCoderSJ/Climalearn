b5ced8ae30540661a3878ebf1f149adc7435f157
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2022-07-23 19:32:13 +0530

    get location

--- App.js
+++ App.js
@@ -13,17 +13,40 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
 import { AppNavigator } from './navigation';
 import 'react-native-gesture-handler';
+import * as Location from 'expo-location';
+
+console.disableYellowBox = true;
 
 const set = async (key, value) => { try { await AsyncStorage.setItem(key, value) } catch (e) { console.log(e) } }
 const setObj = async (key, value) => { try { const jsonValue = JSON.stringify(value); await AsyncStorage.setItem(key, jsonValue) } catch (e) { console.log(e) } }
 const get = async (key) => { try { const value = await AsyncStorage.getItem(key); if (value !== null) { try { return JSON.parse(value) } catch { return value } } } catch (e) { console.log(e) } }
 
 let moveOn = false
+let locationNotGranted = false
 
 let width = Dimensions.get('window').width-100
 
 export default () => {
   const [, forceUpdate] = React.useReducer(x => x + 1, 0);
+
+  const requestLocationPermission = () => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        locationNotGranted = true;
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.warn(location)
+      setObj('location', {
+        lat: location.coords.latitude,
+        long: location.coords.longitude
+      })
+      moveOn = true
+      locationNotGranted = false;
+    })();
+  }
 
   get("readIntro")
   .then(val => {
@@ -33,11 +56,19 @@ export default () => {
     }
   })
 
+  get("location")
+  .then(val => {
+    if (!val) {
+      locationNotGranted = true
+      forceUpdate()
+    }
+  })
+
   return(
   <>
     <IconRegistry icons={EvaIconsPack}/>
     <ApplicationProvider {...eva} theme={eva.dark}>
-    {!moveOn &&
+    {!moveOn && !locationNotGranted &&
     <Layout style={{flex: 1, justifyContent: 'center', padding: 50}}>
 
       <Image source={require('./assets/UNGoal13.png')} style={{width: width, height: width, marginBottom: 20}} />
@@ -46,12 +77,24 @@ export default () => {
         Each year, the amount of CO2 emitted into the atmosphere increases, and keeps hurting the earth. It has become a global concern, leading the UN to make it one of their 17 Sustainability Development Goals. Goal 13 task 3 (13.3) is to educate the general public about the effects of CO2 on the environment. This app aims to do just that. With Climalearn, you can see what changes the US Department of Energy predicts to the city you live in, and find out how you can help slow this down for a better future.
       </Text>
 
-      <Button onPress={() => {set("readIntro", "true"); moveOn = true; forceUpdate()}}>
+      <Button onPress={() => {set("readIntro", "true"); moveOn = true; requestLocationPermission(); forceUpdate()}}>
         <Text>Get Started</Text>
       </Button>
     </Layout>
     }
-    {moveOn &&
+    {locationNotGranted && moveOn &&
+    <Layout style={{flex: 1, justifyContent: 'center', padding: 50}}>
+
+      <Text category="h4" style={{marginBottom: 20}}>
+        Uh oh! You denied the app access to your location. We use your location to show location based predictions and solutions. Your location is stored only on the device.
+      </Text>
+
+      <Button onPress={() => {requestLocationPermission();}}>
+        <Text>Allow Location Access</Text>
+      </Button>
+    </Layout>
+    }
+    {moveOn && !locationNotGranted &&
       <AppNavigator/>
     }
     </ApplicationProvider>
